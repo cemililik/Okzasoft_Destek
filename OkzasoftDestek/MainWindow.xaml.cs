2ef74eb874a4a81b53d173a04a3087963c4b1f78
Cemil ILIK <cemililik@outlook.com>
2018-09-26 16:01:53 +0300

    interfaces, user_controls

---
+++ OkzasoftDestek/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using OkzasoftDestek.Kontroller;
+using OkzasoftDestek.sinif;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace OkzasoftDestek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKapat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void brdRightTop_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void hizmetOzeti_Click(object sender, RoutedEventArgs e)
+        {
+            uc_Cagir.ucEkle(masterGrid, new uc_Ozet());
+        }
+
+        private void btnKurumlar_Click(object sender, RoutedEventArgs e)
+        {
+            uc_Cagir.ucEkle(masterGrid, new uc_Kurumlar());
+        }
+
+        private void btnKisi_Click(object sender, RoutedEventArgs e)
+        {
+            uc_Cagir.ucEkle(masterGrid, new uc_Kisiler());
+        }
+
+        private void btnEkipman_Click(object sender, RoutedEventArgs e)
+        {
+            uc_Cagir.ucEkle(masterGrid, new uc_Ekipman());
+        }
+    }
+}
---
+++ OkzasoftDestek/sinif/uc_Cagir.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OkzasoftDestek.sinif
+{
+    public class uc_Cagir
+    {
+        public static void ucEkle(Grid grd, UserControl uc)
+        {
+            if (grd.Children.Count > 0)
+            {
+                grd.Children.Clear();
+                grd.Children.Add(uc);
+            }
+            else
+            {
+                grd.Children.Add(uc);
+            }
+        }
+    }
+}
